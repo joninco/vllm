2c703816b7d85f998bd7dacaa2106e78068109b0
Nick Hill <nickhill123@gmail.com>
2026-05-19 19:19:05 -0700

    [MRV2][BugFix] Fix default-stream CG capture in P/W LoRA case (#43160)
    Signed-off-by: Nick Hill <nickhill123@gmail.com>

--- vllm/v1/worker/gpu/cudagraph_utils.py
+++ vllm/v1/worker/gpu/cudagraph_utils.py
@@ -365,6 +365,7 @@ class ModelCudaGraphManager(CudaGraphManager):
                     batch_descriptor = BatchDescriptor(
                         num_tokens=num_tokens,
                         uniform=desc.uniform_token_count == self.decode_query_len,
+                        has_lora=has_lora,
                     )
                 with set_forward_context(
                     attn_metadata,
